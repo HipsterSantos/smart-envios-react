04fa506c89c2a4215a57a3c3e8fbc0894912cf9f
HipsterSantos <santoscampos269@gmail.com>
2021-11-12 21:57:00 +0100

    structuring the application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+const logger = process.env.API_TOKEN;
+console.log('logger value ',logger);
 const TesComponent = ()=>{
   return ( <div>
     <h1>Hey , I'm test component </h1>
