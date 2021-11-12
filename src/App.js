1eae9d7e0dc3c561379b88e5cb33900a57bfbd4d
HipsterSantos <santoscampos269@gmail.com>
2021-11-12 03:28:13 +0100

    fixing some error in pre-set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 
+const TesComponent = ()=>{
+  return ( <div>
+    <h1>Hey , I'm test component </h1>
+    </div>)
+}
+
 const App = () => (
     <div>
         <h1>Hello React</h1>
+        <TesComponent/>
     </div>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-const HtmlWebPlugin  = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry:'./src/index.js',
-  output:{
-    filename:'bundle.[hash].js',
-    path:path.resolve(_dirname,'dist'),
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
   },
-  plugins:[
-    new HtmlWebPlugin({
-      template: './src/index.html',
-    })
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
   ],
-  resolve:{
-    modules:[_dirname,"src","node_modules"],
-    extensions: [ "*",".js","jsx","jsx",".tsx",".ts"],
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
   },
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
-        test:/\.jsx?$/,
-        include: /node_modules/,
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
         loader: require.resolve("babel-loader"),
-      },{
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
       {
         test: /\.png|svg|jpg|gif$/,
         use: ["file-loader"],
-      }
-    ]
-  }
-
-}
+      },
+    ],
+  },
+};
